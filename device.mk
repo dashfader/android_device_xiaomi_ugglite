fad34e2227f19d36b870945e7f93b9ce52c654c6
andeh24 <fdlandriyanto@gmail.com>
2020-09-27 05:15:36 +0700

    Revert "ugglite: shim nougat prebuilt HAL"
    This reverts commit e60b1bdf5fc33e62d9c45834df39f87dfd2b25d6.

--- device.mk
+++ device.mk
@@ -155,10 +155,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
-# shims
-PRODUCT_PACKAGES += \
-    libdisplayreceiver
-
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
